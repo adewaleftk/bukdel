b3cd96848b53726032204a9f7ba4c32031b39ed0
Adewale Fatoki <adewaleftk@gmail.com>
2023-08-16 02:33:15 +0100

    logistics estimate added

--- src/assets/components/Services.jsx
+++ src/assets/components/Services.jsx
@@ -15,28 +15,28 @@ function Services() {
                 <h3>Bukdel Delicacies</h3>
                 <p>Order delicious chef-prepared meals you&apos;ll</p>
                 <p>enjoy and get it delivered on time.</p>
-                <Link to='/delicacies'>Get Started &#8594;</Link>
+                <p><Link to='/delicacies'>Get Started </Link></p>
             </div>
             <div>
                 <img src={Logistics} alt="Bukdel Logistics"/>
                 <h3>Bukdel Logistics</h3>
                 <p>Order delicious chef-prepared meals you&apos;ll</p>
                 <p>enjoy and get it delivered on time.</p>
-                <Link to='logistics'>Get Started &#8594;</Link>
+                <p><Link to='logistics'>Get Started</Link></p>
             </div>
             <div>
                 <img src={Events} alt="Bukdel Events" />
                 <h3>Bukdel Events</h3>
                 <p>Order delicious chef-prepared meals you&apos;ll</p>
                 <p>enjoy and get it delivered on time.</p>
-                <p>Get Started &#8594;</p>
+                <p>Get Started</p>
             </div>
             <div>
                 <img src={Drinks} alt="Bukdel Drinks"/>            
                 <h3>Bukdel Drinks</h3>
                 <p>Order delicious chef-prepared meals you&apos;ll</p>
                 <p>enjoy and get it delivered on time.</p>
-                <p>Get Started &#8594;</p>
+                <p>Get Started</p>
             </div>
         </div>
     </div>
